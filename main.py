f058fce5a4502808e89a7a2dcfbcb53fc40f35aa
TotoCode <118753153+TotoCodeFR@users.noreply.github.com>
2024-02-28 11:38:00 +0100

    Made home menu as a function (HomeMenu), finished RDRead function.

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-SelectedHomeOption = "Radio"
+def on_button_pressed_ab():
+    global Page, SelectedOption
+    if Page == "Home":
+        if SelectedOption == "Radio":
+            Page = "Radio"
+            SelectedOption = "Read"
+            Kitronik_VIEWTEXT32.clear_display()
+            Kitronik_VIEWTEXT32.display_single_line_string(Kitronik_VIEWTEXT32.DisplayLine.TOP, "     Radio      ")
+            Kitronik_VIEWTEXT32.display_single_line_string(Kitronik_VIEWTEXT32.DisplayLine.BOTTOM, "<     Read     >")
+    if Page == "Radio":
+        if SelectedOption == "Read":
+            Page = "RDRead"
+            SelectedOption = ""
+            Kitronik_VIEWTEXT32.clear_display()
+            Kitronik_VIEWTEXT32.display_single_line_string(Kitronik_VIEWTEXT32.DisplayLine.TOP, "     Radio      ")
+            Kitronik_VIEWTEXT32.display_single_line_string(Kitronik_VIEWTEXT32.DisplayLine.BOTTOM, "<     Read     >")
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+SelectedOption = ""
+Page = ""
+Page = "Home"
+SelectedOption = "Radio"
 Kitronik_VIEWTEXT32.clear_display()
 Kitronik_VIEWTEXT32.display_single_line_string(Kitronik_VIEWTEXT32.DisplayLine.TOP, "  Flipper:bit   ")
-Kitronik_VIEWTEXT32.display_single_line_string(Kitronik_VIEWTEXT32.DisplayLine.BOTTOM, "Radio")
+Kitronik_VIEWTEXT32.display_single_line_string(Kitronik_VIEWTEXT32.DisplayLine.BOTTOM, "<    Radio     >")
 
 def on_forever():
     pass
--- main.ts
+++ main.ts
@@ -1,17 +1,51 @@
+function HomeMenu () {
+    StopRadioReading = 0
+    Page = "Home"
+    SelectedOption = "Radio"
+    Kitronik_VIEWTEXT32.clearDisplay()
+    Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, "  Flipper:bit   ")
+    Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Bottom, "<    Radio     >")
+}
 input.onButtonPressed(Button.AB, function () {
     if (Page == "Home") {
         if (SelectedOption == "Radio") {
-        	
+            Page = "Radio"
+            SelectedOption = "Read"
+            Kitronik_VIEWTEXT32.clearDisplay()
+            Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, "     Radio      ")
+            Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Bottom, "<     Read     >")
+        }
+    } else {
+        if (Page == "Radio") {
+            if (SelectedOption == "Read") {
+                Page = "RDRead"
+                SelectedOption = ""
+                Kitronik_VIEWTEXT32.clearDisplay()
+                Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, "   Read Radio   ")
+                Kitronik_VIEWTEXT32.scrollString(Kitronik_VIEWTEXT32.DisplayLine.Bottom, "Reading Radio Signal, turn it on!")
+                RadioChannel = 0
+                for (let index = 0; index <= 999; index++) {
+                    radio.setGroup(index)
+                    RadioChannel = index
+                    control.waitMicros(500000)
+                    if (StopRadioReading == 1) {
+                        break;
+                    }
+                }
+            }
         }
     }
 })
+radio.onReceivedValue(function (name, value) {
+    StopRadioReading = 1
+    Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, "   Radio Scan   ")
+    Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Bottom, "Found Signal!")
+})
+let RadioChannel = 0
 let SelectedOption = ""
 let Page = ""
-Page = "Home"
-SelectedOption = "Radio"
-Kitronik_VIEWTEXT32.clearDisplay()
-Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, "  Flipper:bit   ")
-Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Bottom, "<    Radio     >")
+let StopRadioReading = 0
+HomeMenu()
 basic.forever(function () {
 	
 })
